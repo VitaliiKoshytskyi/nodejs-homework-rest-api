2026f2178e454933cea2ac286271b9a26983fdaf
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-05-07 14:53:59 +0300

    updated markup

---
+++ helpers/createVerifyEmail.js
@@ -0,0 +1,12 @@
+// const { BASE_URL } = process.env;
+
+// const createVerifyEmail = (verificationToken) => {
+//      const verifyLink = `${BASE_URL}/users/verify/${verificationToken}`
+//     const verifyEmail = {
+//       to: email,
+//       subject: "Verify email",
+//       html:`<a target="_blank" href="${verifyLink}">Click to verify email</a>`
+//     }
+//     return verifyEmail
+// }
+// module.exports = createVerifyEmail;
